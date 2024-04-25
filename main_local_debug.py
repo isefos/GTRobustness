7a2a2143fc426b4a300c885daec67802757bec2e
Philipp Foth <philipp.foth@tum.de>
2024-04-25 19:15:02 +0200

    reorganized the local test configs

--- main_local_debug.py
+++ main_local_debug.py
@@ -7,7 +7,7 @@ torch.autograd.set_detect_anomaly(True, check_nan=True)
 
 
 
-with open("configs_sacred/test_cora.yaml") as f:
+with open("configs_sacred/GCN/upfd_pol.yaml") as f:
     cfg = yaml.safe_load(f)
 
 r = ex.run(config_updates=cfg)
